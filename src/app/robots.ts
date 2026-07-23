a016a33b18c6eef282ff28d2119961fd42f986aa
venkatavivek <venkatavivek@example.com>
2026-07-23 17:17:12 +0530

    seo: update robots.txt rules for crawlers and private paths

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -18,10 +18,59 @@ export default function robots(): MetadataRoute.Robots {
         ],
       },
       {
-        userAgent: 'GPTBot',
-        disallow: '/',
+        userAgent: 'Googlebot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Googlebot-Image',
+        allow: '/',
+      },
+      {
+        userAgent: 'Googlebot-News',
+        allow: '/',
+      },
+      {
+        userAgent: 'Googlebot-Video',
+        allow: '/',
+      },
+      {
+        userAgent: 'AdsBot-Google',
+        allow: '/',
+      },
+      {
+        userAgent: 'AdsBot-Google-Mobile',
+        allow: '/',
+      },
+      {
+        userAgent: 'Mediapartners-Google',
+        allow: '/',
+      },
+      {
+        userAgent: 'Storebot-Google',
+        allow: '/',
+      },
+      {
+        userAgent: 'Bingbot',
+        allow: '/',
+      },
+      {
+        userAgent: 'DuckDuckBot',
+        allow: '/',
+      },
+      {
+        userAgent: 'Slurp',
+        allow: '/',
+      },
+      {
+        userAgent: 'Yandex',
+        allow: '/',
+      },
+      {
+        userAgent: 'Baiduspider',
+        allow: '/',
       },
     ],
     sitemap: 'https://chalapathipharmacy.ac.in/sitemap.xml',
   };
 }
+
